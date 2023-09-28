1eebd1986f027eff3899e84e8dcaa55a23475434
Enrique Sanchez <esanchez.devel@gmail.com>
2023-09-28 21:08:33 +0200

    feat: basic pulsar producer and consumer

---
+++ src/main/java/com/esanchez/devel/pulsar/PulsarConsumer.java
@@ -0,0 +1,70 @@
+package com.esanchez.devel.pulsar;
+
+import org.apache.pulsar.client.api.Consumer;
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
+
+public class PulsarConsumer {
+
+	private static final String PULSAR_HOST = "pulsar://localhost:6650";
+	private static final String PULSAR_TOPIC = "test-topic";
+	private static final String PULSAR_SUBSCRIPTION = "test-consumer-subscription";
+	
+	public static void main(String[] args) {
+		System.out.println("Pulsar Consumer");
+		System.out.println("~~~~~~~~~~~~~~~");
+		System.out.println();
+		
+		PulsarClient client = null;
+		try {
+			client = PulsarClient.builder()
+			        .serviceUrl(PULSAR_HOST)
+			        .build();
+		} catch (PulsarClientException e) {
+			System.out.println("Error initializing Pulsar Client");
+			e.printStackTrace();
+		}
+		
+		if (client != null) {
+			Consumer consumer = null;
+			try {
+				consumer = client.newConsumer()
+				        .topic(PULSAR_TOPIC)
+				        .subscriptionName(PULSAR_SUBSCRIPTION)
+				        .subscribe();
+				
+				while (true) {
+					  // Wait for a message
+					  Message msg = consumer.receive();
+
+					  try {
+					      System.out.println("Message received: " + new String(msg.getData()));
+
+					      // Acknowledge the message
+					      consumer.acknowledge(msg);
+					  } catch (Exception e) {
+						  System.out.println("Message failed to process. Redeliver later");
+					      consumer.negativeAcknowledge(msg);
+					  }
+					}
+			} catch (PulsarClientException e) {
+				System.out.println("Error consuming message");
+				e.printStackTrace();
+			} finally {
+				try {
+					if (consumer != null) {
+						consumer.close();
+					}
+					
+					if (client != null) {
+						client.close();
+					}
+				} catch (PulsarClientException e) {
+					System.out.println("Error closing Pulsar Producer and Client");
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
---
+++ src/main/java/com/esanchez/devel/pulsar/PulsarProducer.java
@@ -0,0 +1,58 @@
+package com.esanchez.devel.pulsar;
+
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
+
+public class PulsarProducer {
+
+	private static final String PULSAR_HOST = "pulsar://localhost:6650";
+	private static final String PULSAR_TOPIC = "test-topic";
+	
+	public static void main(String[] args) {
+		System.out.println("Pulsar Producer");
+		System.out.println("~~~~~~~~~~~~~~~");
+		System.out.println();
+		
+		PulsarClient client = null;
+		try {
+			client = PulsarClient.builder()
+			        .serviceUrl(PULSAR_HOST)
+			        .build();
+		} catch (PulsarClientException e) {
+			System.out.println("Error initializing Pulsar Client");
+			e.printStackTrace();
+		}
+		
+		if (client != null) {
+			Producer<byte[]> producer = null;
+			try {
+				producer = client.newProducer()
+				        .topic(PULSAR_TOPIC)
+				        .create();
+				
+				// You can then send messages to the broker and topic you specified:
+				String message = "This is a test message sent from JAVA client";
+				producer.send(message.getBytes());
+				
+				System.out.println("Message sent to Pulsar");
+			} catch (PulsarClientException e) {
+				System.out.println("Error producing message");
+				e.printStackTrace();
+			} finally {
+				try {
+					if (producer != null) {
+						producer.close();
+					}
+					
+					if (client != null) {
+						client.close();
+					}
+				} catch (PulsarClientException e) {
+					System.out.println("Error closing Pulsar Producer and Client");
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
