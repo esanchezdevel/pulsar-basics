9981a8e5c457f0aa561ab3a08c4b74c2c6c73c31
Enrique Sanchez <esanchez.devel@gmail.com>
2023-10-01 14:19:58 +0200

    feat: create tenant and namespace in producer. Not working yet

--- src/main/java/com/esanchez/devel/pulsar/PulsarProducer.java
+++ src/main/java/com/esanchez/devel/pulsar/PulsarProducer.java
@@ -1,15 +1,25 @@
 package com.esanchez.devel.pulsar;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.pulsar.client.admin.PulsarAdmin;
 import org.apache.pulsar.client.api.Producer;
 import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.api.PulsarClientException;
 import org.apache.pulsar.client.api.Schema;
+import org.apache.pulsar.common.policies.data.TenantInfo;
+import org.apache.pulsar.common.policies.data.TenantInfoImpl;
 
 public class PulsarProducer {
-
 	
+	private static final String PULSAR_ADMIN_HOST = "http://localhost:8080";
+	private static final String PULSAR_ADMIN_ROLE = "admin-roles";
+	private static final String PULSAR_CLUSTER = "test-cluster";
 	private static final String PULSAR_HOST = "pulsar://localhost:6650";
-	private static final String PULSAR_TOPIC = "test-topic";
+	private static final String PULSAR_TENANT = "test-tenant";
+	private static final String PULSAR_NAMESPACE = "test-namespace";
+	private static final String PULSAR_TOPIC = "persistent://" + PULSAR_TENANT + "/" + PULSAR_NAMESPACE + "test-topic";
 	
 	public static void main(String[] args) {
 		System.out.println("Pulsar Producer");
@@ -27,6 +37,34 @@ public class PulsarProducer {
 		}
 		
 		if (client != null) {
+			
+			// Create tenant and namespace if they not exists
+			try {
+				PulsarAdmin admin = PulsarAdmin.builder()
+						.serviceHttpUrl(PULSAR_ADMIN_HOST)
+						.build();
+				
+				if (!admin.tenants().getTenants().contains(PULSAR_TENANT)) {
+					
+					Set<String> adminRoles = new HashSet<>();
+					adminRoles.add(PULSAR_ADMIN_ROLE);
+					
+					Set<String> clusters = new HashSet<>();
+					clusters.add(PULSAR_CLUSTER);
+					
+					TenantInfo tenantInfo = new TenantInfoImpl(adminRoles, clusters);
+					
+					admin.tenants().createTenant(PULSAR_TENANT, tenantInfo);	    
+				}
+				
+				if (!admin.namespaces().getNamespaces(PULSAR_TENANT).contains(PULSAR_NAMESPACE)) {
+					admin.namespaces().createNamespace(PULSAR_TENANT + "/" + PULSAR_NAMESPACE);
+				}
+			} catch (Exception e) {
+				System.out.println("Error requesting admin information");
+				e.printStackTrace();
+			}
+			
 			Producer<String> producer = null;
 			try {
 				// If no tenant and namespace are defined, the topic will be created in
